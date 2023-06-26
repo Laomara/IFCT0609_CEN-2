7a541535336d21d35b4be74f2e7d95417dc251bd
Laura S Ferris <128062359+Laomara@users.noreply.github.com>
2023-06-26 09:24:01 +0200

    An1adimos express Router para los endpoints

--- Node/Express/2.Tutorial/app.js
+++ Node/Express/2.Tutorial/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
+const router = require('./router');
 
-app.get('/', function(req, res){
-   res.send("Hola mundo desde express");
-});
+// Es posible anidar todas las rutas de router en este paso:
+app.use('/', router);
 
-app.listen(3000);
+app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
