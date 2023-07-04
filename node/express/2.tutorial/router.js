448d7eaf0cc2b9df0686f2f8f9c977fa6471cc07
Laura S Ferris <128062359+Laomara@users.noreply.github.com>
2023-07-04 12:22:56 +0200

    Refresh and update

--- node/express/2.tutorial/router.js
+++ node/express/2.tutorial/router.js
@@ -2,19 +2,11 @@ const express = require('express');
 const router = express.Router();
 const urlencode = require('urlencode'); // librería para codificar y decodificar URLs
 
-const path = require('path'); // Módulo para crear rutas absolutas a nuestros archivos
-
-// Body parser para leer el body de los requests -> POST, PUT
 const bodyParser = require('body-parser');
-// To parse URL encoded data
-router.use(bodyParser.urlencoded({ extended: false }))
-// body -> JSON
+//To parse URL encoded data
+router.use(bodyParser.urlencoded({extended: false}))
+//body -> JSON
 router.use(bodyParser.json())
-
-// Para servir archivos estáticos
-// Nota: a diferencia del tutorial, nosotros usamos router en vez de app:
-router.use("/img", express.static('public'));
-// Las imágenes estarán disponibles en localhost:3000/img/<nombre_de_archivo.extension>
 
 const notas = [
   {id: 1, texto: "Nota 1", name: "Pepe"},
@@ -23,9 +15,9 @@ const notas = [
   {id: 2, texto: "Nota 2", name: "María"},
 ]
 const users = [
-  {id: 0, name: "Pepe", email: "pepe@gmail.com"},
-  {id: 1, name: "María", email: "maria@gmail.com"},
-  {id: 2, name: "Juan", email: "juan@gmail.com"},
+  {id: "0", name: "Pepe", email: "pepe@gmail.com"},
+  {id: "1", name: "María", email: "maria@gmail.com"},
+  {id: "2", name: "Juan" , email: "juan@gmail.com"},
 ]
 // Middleware - console.log de la hora, la ruta, la ip y el método de cada request
 // Middleware #1
@@ -38,37 +30,22 @@ router.use(function (req, res, next) {
   next();
   // TODO: Ejercicio. Guardar en un archivo de texto la hora, la ruta, la ip y el método de cada request -> TXT
 });
-// Middleware #2 - cambia req.method al valor correcto si viene en un form
+// Middleware #2
 router.use(function (req, res, next) {
-  let metodoOriginal = req.method;
-  if (req.body && req.body._method) {
-    req.method = req.body._method.toUpperCase(); // put -> PUT
-    delete req.body._method;
-  }
-  console.log("Método original:" + metodoOriginal + " Método actual: " + req.method);
+    let fechaHora = new Date().toLocaleString();
+  console.log("- Hora en el Middleware #2");
+  //Escribimos el usuario (con id y name) que pasamos en em middleware #1
+  console.log(`
+   Usuario:
+   id: ${req.user.id},
+   nombre: ${req.user.name}
+   `)
+   req.user.rol = "usuario" // AÑadimos una propiedad al objeto user
   next();
 });
 router.all('/', function(req, res){
   console.log("Hola desde /"); // Después de ejecutar el middleware #1 y #2
-  res.send(`
-  <h1>Endpoints de usuarios</h1>
-  <h2>JSON</h2>
-  <ul>
-    <li><code>GET</code> <a href="/json/usuarios">/json/usuarios</a> <span>Lista de todos los usuarios</span></li>
-    <li><code>GET</code> <a href="/json/usuarios/1">/json/usuarios/&lt;id&gt;</a> <span>Usuario por <code>id</code> (int)</span></li>
-    <li><code>POST</code> <a href="/json/crear">/json/crear</a> <span>Crear usuario</span></li>
-    <li><code>PUT</code> <a href="/json/editar/1">/json/editar/&lt;id&gt;</a> <span>Editar usuario por <code>id</code> (int)</span></li>
-    <li><code>DELETE</code> <a href="/json/borrar/1">/json/borrar/&lt;id&gt;</a> <span>Borrar usuario por <code>id</code> (int)</span></li>
-  </ul>   
-  <h2>HTML</h2>
-  <ul>
-    <li><code>GET</code> <a href="/html/usuarios">/html/usuarios</a> <span>Lista de todos los usuarios</span></li>
-    <li><code>GET</code> <a href="/html/usuarios/1">/html/usuarios/&lt;id&gt;</a> <span>Usuario por <code>id</code> (int)</span></li>
-    <li><code>POST</code> <a href="/html/crear">/html/crear</a> <span>Crear usuario</span></li>
-    <li><code>PUT</code> <a href="/html/editar/1">/html/editar/&lt;id&gt;</a> <span>Editar usuario por <code>id</code> (int)</span></li>
-    <li><code>DELETE</code> <a href="/html/borrar/1">/html/borrar/&lt;id&gt;</a> <span>Borrar usuario por <code>id</code> (int)</span></li>
-  </ul> 
-  `);
+  res.send("Hola mundo desde express. Usuario actual: " + req.user.name + " con id " + req.user.id + " con rol " + req.user.rol);
 });
 // El ? después del parámetro name indica que es opcional, entonces podemos usar el endpoint en caso de recibirlo o no
 router.get('/hola/:name?', function(req, res){
@@ -79,115 +56,81 @@ router.get('/hola/:name?', function(req, res){
   }
 });
 router.post('/hola', function(req, res){
-  // TODO: crear el usuario en la base de datos
-  let nuevoUsuario = req.body; // req.body es el objeto que enviamos en el body del request al hacer un POST
-  nuevoUsuario.rol = "usuario"
-  res.send(nuevoUsuario);
+    // TODO: crear el usuario en la base de datos
+    let nuevoUsuario = req.body; // req.body es el objeto que enviamos en el body del request al hacer un POST
+    nuevoUsuario.rol = 'usuario'
+    res.send(nuevoUsuario);
 });
 
 // ****************** Usuarios GET y GET por id ******************
 // Pattern matched routes
-router.get("/html/usuarios", function(req, res){
-  res.sendFile(path.join(__dirname, "html", "usuarios.html"));
-});
-router.get("/json/usuarios", function(req, res){
-  res.send(users) // JSON
-});
-router.get("/html/usuarios/:id(\\d+)", function(req, res){
-  res.sendFile(path.join(__dirname, "html", "usuario.html"));
-});
-router.get('/json/usuarios/:id(\\d+)', function(req, res){
-  // \\d+ es un regex para validar dígitos en cualuier cantidad
-  let user = users.find(user => user.id == req.params.id);
-  res.send(user);
-});
-// ******* Formulario para crear un usuario *******
-router.get('/html/crear', function(req, res){
-  // Formulario para crear un usuario
-  let ubicacion = 'forms/form-crear.html';
-  // Usamos path para obtener ruta a la carpeta actual y concatenarle la ruta relativa del archivo
-  console.log("Ubicación absoluta de la carpeta: " + path.join(__dirname, ""));
+router.get("/usuarios", function(req, res){
+    res.send(users)
+  });
+  router.get('/usuarios/:id([0-9]{1})', function(req, res){
+    let user = users.find(user => user.id == req.params.id);
+    res.send(user);
+  });
+  // ******* Formulario para crear un usuario *******
+  router.get('/crear', function(req, res){
+// Form para crear un usuario
+let ubicacion = 'forms/form-crear.html'; 
+// Usamos path para obtener ruta a la carpeta actual y concatenarle la ruta relativa del archivo
+console.log("Ubicación absoluta de la carpeta: " + path.join(__dirname, ""));
   console.log("Ubicación relativa del archivo: " + ubicacion);
-  // Colocamos una cookie que guarde el tamaño del array de users actualmente:
-  res.cookie('__usersLength', users.length);
-  // Devolvemos en el response el archivo html del formulario
-  res.sendFile(path.join(__dirname, ubicacion))
+// Devolvemos en el response el archivo html del formulario
+res.sendFile(path.join(__dirname, ubicacion))
+}); 
+router.post('/crear', function(req, res){
+    // Recibimos el form y lo enviamos de vuelta
+    let nuevoUsuario = req.body; // req.body es el objeto que enviamos en el body del request al hacer un POST
+    let nuevoId = users.length;
+    nuevoUsuario.id = nuevoId; // 3, 4, 5...
+    let nuevoUsuarioIdx = users.push(nuevoUsuario); // Actualizamos variable con el dato DESDE el array y lo devolvemos para comprobar que se añadió con todos los campos
+    console.log(users[nuevoUsuarioIdx - 1])
+    // TODO: crear el usuario en la base de datos
+    res.status(201); // 201 Created
+    res.send(users[nuevoUsuarioIdx - 1]); // o el user nuevo
+  });
+  
+  // TODO: form y endpoint para editar un usuario -> GET y PUT
+  router.get('/editar/:id', function(req, res){
+    let ubicacion = 'forms/form-editar.html';
+    res.cookie("__id", req.params.id);
+    // Form para editar un usuario
+    res.sendFile(path.join(__dirname, ubicacion))
+  });
+  // Revisar porque con PUT no se envia el form
+  router.post('/editar/:id', function(rq,res){
+    // editar   
+    res.send("Editar usuario con id " + req.params.id);
+  });
+  // TODO: form y endpoint para borrar un usuario -> GET y DELETE
+  router.delete('/borrar/:id', function(req, res){
+    // borrar
+    res.send("Borrar usuario con id " + req.params.id);
+  });
+  
+  // *********************** Usuarios ***********************
+  
+  // Notas de cosas por hacer
+  // Final de formulario para crear un usuario
+  router.get('/notas', function(req, res){
+    res.send(notas);   
 });
-router.post('/json/crear', function(req, res){
-  // Recibimos el form y lo enviamos de vuelta
-  let nuevoUsuario = req.body; // req.body es el objeto que enviamos en el body del request al hacer un POST
-  let nuevoUsuarioIdx = users.push(nuevoUsuario); // Actualizamos variable con el dato DESDE el array y lo devolvemos para comprobar que se añadió con todos los campos
-  console.log(users[nuevoUsuarioIdx - 1])
-  // TODO: crear el usuario en la base de datos
-  res.status(201); // 201 Created
-  res.send(users[nuevoUsuarioIdx - 1]); // o el user nuevo
-});
-//  ********* Formulario para editar un usuario *********
-router.get('/html/editar/:id(\\d+)', function(req, res){
-  let ubicacion = 'forms/form-editar.html';
-  res.cookie("__id", req.params.id); // Guardamos el id para leerlo desde el HTML, de la misma forma, podemos guardar el nombre, email, etc.
-  console.log(req.params)
-  // Formulario para editar un usuario
-  res.sendFile(path.join(__dirname, ubicacion))
-});
-// Nota: revisar por qué con PUT el form no se envía
-router.put('/editar/:id(\\d+)', function(req, res){
-  let id = req.params.id;
-  let usuarioEditado = req.body;
-  // Buscamos el id del usuario en el array
-  let usuarioIdx = users.findIndex(user => user.id == id);
-  if(usuarioIdx == -1){
-    usuarioIdx = users.length; // Si no existe, lo añadimos al final
-  }
-  // Actualizamos el usuario en el array
-  users[usuarioIdx] = usuarioEditado;
-  // Devolvemos el usuario actualizado
-  res.send(users[usuarioIdx]);
-});
-// ********* Formulario para borrar un usuario *********
-router.get('/html/borrar/:id(\\d+)', function(req, res){
-  let ubicacion = 'forms/form-borrar.html';
-  res.sendFile(path.join(__dirname, ubicacion))
-});
-router.delete('/json/borrar/:id(\\d+)', function(req, res){
-  let id = req.params.id;
-  let idx = users.findIndex(user => user.id == id); // Si no encuentra el id, devuelve -1
-  if(idx == -1){
-    res.status(404); // 404 Not Found
-    let ubicacion = 'html/404.html';
-    res.sendFile(path.join(__dirname, ubicacion));
-  }else{
-    users.splice(idx, 1); // Borramos el usuario del array
-    res.send(`Usuario con id ${id} borrado`);
-  }
-});
-// *********************** Usuarios ***********************
 
-// Notas de cosas por hacer
-// Final de formulario para crear un usuario
-router.get('/notas', function(req, res){
-  res.send(notas);
-});
 // URL Building con Express
 // Route parameters
-router.get('/notas/:id(\\d+)', function(req, res){
+router.get('/notas/:id', function(req, res){
   let nota = notas.find(nota => nota.id == req.params.id);
   res.send(nota);
 });
 // TODO: Ejercicio. Crear un solo enpoint que use un parámetro opcional para filtrar por nombre y por id, es decir, "fusionar" los dos endpoints /:id y /:id/:name
-router.get('/notas/:id(\\d+)/:name', function(req, res){
+router.get('/notas/:id/:name', function(req, res){
   let nota = notas.find(nota => nota.id == req.params.id && nota.name == req.params.name);
   res.send(nota);
 });
 
-// *********************** Imágenes ***********************
-// Imágenes en carpeta 'public'
-router.get("/img", function(req, res){
-  let ubicacion = 'html/img.html';
-  res.sendFile(path.join(__dirname, ubicacion));
-});
-
-// Todas las demás rutas y métodos no implementados
 router.all('*', function(req, res){
   res.status(404); // HTTP status 404: NotFound
   res.send("404 Not Found. Ruta o método no están implementados");
